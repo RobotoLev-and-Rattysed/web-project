d58c60b20db90fd1ec595667f6285e4c34c79be9
Programmer <robotolev@gmail.com>
2020-04-20 23:00:36 +0300

    Настройка ДС-бота

--- discord_handler.py
+++ discord_handler.py
@@ -1,5 +1,4 @@
 import discord
-import requests
 
 
 class YLBotClient(discord.Client):
@@ -9,16 +8,7 @@ class YLBotClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if any([word in message.content.lower()
-                for word in {'кот', 'cat', 'кошк', 'коша', 'кис', 'кыс'}]):
-            request = requests.get('https://api.thecatapi.com/v1/images/search')
-            url = request.json()[0]['url']
-            await message.channel.send(url)
-        elif any([word in message.content.lower()
-                  for word in {'пес', 'пёс', 'собак', 'собач', 'щен', 'dog'}]):
-            request = requests.get('https://dog.ceo/api/breeds/image/random')
-            url = request.json()['message']
-            await message.channel.send(url)
+        await message.channel.send(f'Я получил сообщение {message.content}')
 
 
 TOKEN = "NzAxODc5ODU0NTIxNjQ3MTI0.Xp362w.ZUBqA0iw3GKsLSnKRe6KbdX_A_U"
