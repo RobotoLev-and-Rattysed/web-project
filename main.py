54dc7be47c5c0eb41686b70dd2da1d4c1af2dc5a
Programmer <robotolev@gmail.com>
2020-04-20 21:27:47 +0300

    Инициализация репозитория + попытка настроить инфраструктуру

---
+++ main.py
@@ -0,0 +1,19 @@
+import flask
+from flask import Flask
+
+import os
+
+
+print('hi!')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run('0.0.0.0', port=port)
---
+++ vk.py
@@ -0,0 +1,22 @@
+import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+
+import random
+
+
+api_token = 'bc1e91d1bdb31d5347620628e04b7cfecf4107c57b1ffd1e2133d8e8a44872db16cbfa953ba2d63ef89e8'
+
+
+vk_session = vk_api.VkApi(token=api_token)
+vk = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session, 194404073)
+
+
+for event in longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        message = event.obj.message
+        from_id = message['from_id']
+        text = message['text']
+
+        vk.messages.send(user_id=from_id, message='Сообщение обработано',
+                         random_id=random.randint(0, 2 ** 64))
