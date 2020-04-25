ec92190c481b7d433dd366268a3727f29754a681
skvortsov <skvortsov.a04@gmail.com>
2020-04-25 16:11:28 +0300

    Продолжена работа с правами.
    -Теперь ноунеймы не могут изменять роль мута.
    -Добавлены системные оповещения от ботов: в случае успеха/ошибки.
    -Всё

--- discord_handler.py
+++ discord_handler.py
@@ -15,17 +15,25 @@ class YLBotClient(discord.Client):
     #     await message.channel.send(f'Я получил сообщение {message.content}')
 
 
-class RandomThings(commands.Cog):
+class MainCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        self.admin_id = ''  # ID админа, который может всё
+        self.admin_id = 0  # ID админа, который может всё
+        self.moderation_role_id = 0  # ID роли модера, который может мутить
         self.mute_role_id = ''  # ID роли мута, которая молчит
 
-    # @commands.command(name='roll_dice')
-    # async def roll_dice(self, ctx, count):
-    #     res = [random.choice(dashes) for _ in range(int(count))]
-    #     await ctx.send(" ".join(res))
+    def has_role(self, member, role_id):
+        for role in member.roles:
+            if role.id == role_id:
+                return True
+        return False
+
+    def is_admin(self, member):
+        return member.id == self.admin_id or member.guild_permissions.administrator
+
+    def is_moderator(self, member):
+        return self.is_admin(member) or self.has_role(member, self.moderation_role_id)
 
     @commands.command(name='randint')
     async def my_randint(self, ctx, min_int, max_int):
@@ -34,19 +42,23 @@ class RandomThings(commands.Cog):
 
     @commands.command(name='muteRole')
     async def set_mute_role(self, ctx, role):
-        print(role)
+        if not self.is_moderator(ctx.author):
+            return await self.perms_error(ctx)
         self.mute_role_id = int(role[3:-1])
         server = ctx.guild.id
         for channel in ctx.guild.text_channels:
-            print(channel)
             await channel.set_permissions(ctx.guild.get_role(self.mute_role_id), send_messages=False)
+        await ctx.message.channel.send('Mute Role has been updated succesfuly!')
         # print(type(ctx.guild.text_channels[0]))
         # print(dir(discord.guild.Guild))
+
+    async def perms_error(self, ctx):
+        await ctx.message.channel.send(f"{ctx.author.mention}, you don't have enought permissions to do that.")
 
 
 TOKEN = "NzAxODc5ODU0NTIxNjQ3MTI0.Xp362w.ZUBqA0iw3GKsLSnKRe6KbdX_A_U"
 bot = commands.Bot(command_prefix='-')
-bot.add_cog(RandomThings(bot))
+bot.add_cog(MainCommands(bot))
 bot.run(TOKEN)
 # client = YLBotClient()
 # client.run(TOKEN)
