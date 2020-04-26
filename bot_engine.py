c11ad7816b7fca17e6c1dd7de580b1a06f592b85
skvortsov <skvortsov.a04@gmail.com>
2020-04-27 02:34:20 +0300

    Начат переход к боту-библиотекарю
    - Теперь бот не бот, а Client
    - Все команды бот забыл, всё откатываем
    - Немного пошаманил в bot_engine, закомментил get_answer, запилил проверку сообщения на наличие команды в нём.
    - Не понял, что в command_engine делает словарь, заменил пока что его на кортеж со строкой, чтобы проверить, что всё робит. Потом изменишь, если надо

--- bot_engine.py
+++ bot_engine.py
@@ -2,20 +2,29 @@ import bots_infrastructure.__all_commands
 from bots_infrastructure.command_engine import bot_commands
 
 
+def is_command(message_text):
+    if len(message_text) < 2 or message_text[0] != '-' or message_text[1:].split()[0] not in bot_commands:
+        return False
+    else:
+        return True
+
+
 def get_answer(message_text, platform):
-    if message_text == "" or message_text[0] != '-':
-        return None, None
-
     command = message_text[1:]
-    if command in bot_commands:
-        if platform in bot_commands[command].platforms:
-            answer_text, answer_attachment = bot_commands[command].action()
-        else:
-            answer_text, answer_attachment = "Команда недоступна на данной платформе", None
-    else:
-        answer_text, answer_attachment = "Такая команда мне не известна", None
+    # if message_text == "" or message_text[0] != '-':
+    #     return None, None
+    #
+    # command = message_text[1:]
+    # if command in bot_commands:
+    #     if platform in bot_commands[command].platforms:
+    #         answer_text, answer_attachment = bot_commands[command].action()
+    #     else:
+    #         answer_text, answer_attachment = "Команда недоступна на данной платформе", None
+    # else:
+    #     answer_text, answer_attachment = "Такая команда мне не известна", None
 
-    if platform == 'vk':
-        return answer_text, answer_attachment
-    else:
-        return answer_text, None
+    # if platform == 'vk':
+    #     return answer_text, answer_attachment
+    # else:
+    #     return answer_text, None
+    pass
--- bots_infrastructure/command_engine.py
+++ bots_infrastructure/command_engine.py
@@ -1,11 +1,11 @@
-bot_commands = {}
+bot_commands = ('help')
 
 
 class BotCommand:
     def __init__(self, keyword, action):
         self.keyword = keyword
         self.action = action
-        bot_commands[self.keyword] = self
+        # bot_commands[self.keyword] = self
 
         self.platforms = set()
         self.description = ''
