06714a0b1b51e3d419e48db5e4bb98c3c354662b
skvortsov <skvortsov.a04@gmail.com>
2020-05-05 19:54:10 +0300

    Исправлен поиск по всему.

--- search.py
+++ search.py
@@ -5,13 +5,19 @@ from bots_infrastructure.command_engine import BotAnswer
 
 def search(name='', author='', genre=''):
     sesion = db_session.create_session()
-    books = sesion.query(Book).join(Author).join(Genre).filter(Book.name.like('%' + name + '%'),
-                                                               Author.name.like('%' + author + '%'),
-                                                               Genre.name.like('%' + genre + '%'),
+    books = sesion.query(Book).join(Author).join(Genre).filter((Book.name.like('%' + name + '%') |
+                                                                Book.name.like('%' + name.capitalize() + '%') |
+                                                                Book.name.like('%' + name.lower() + '%')),
+                                                               (Author.name.like('%' + author + '%') |
+                                                                Author.name.like('%' + author.capitalize() + '%') |
+                                                                Author.name.like('%' + author.lower() + '%')),
+                                                               (Genre.name.like('%' + genre + '%') |
+                                                                Genre.name.like('%' + genre.capitalize() + '%') |
+                                                                Genre.name.like('%' + genre.lower() + '%')),
                                                                Book.status == 1)
     if len(books.all()) == 0:
         return BotAnswer('Книг с таким именем не найдено.')
-    answer = f'Результаты по запросу "{name}"\n'
+    answer = f'Результаты по запросу "{name + author + genre}"\n'
     for book in books:
         answer += f'''Название книги: {book.name}
     Автор: {book.author.name}
