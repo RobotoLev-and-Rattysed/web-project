2a6684e97ef9cef214bf711931494f00d47208ce
skvortsov <skvortsov.a04@gmail.com>
2020-05-05 19:40:50 +0300

    Команды были написаны, но не были добавлены. ИСПРАВЛЕНО!

--- bots_infrastructure/__all_commands.py
+++ bots_infrastructure/__all_commands.py
@@ -2,3 +2,5 @@ from .commands.hello_world import *
 from .commands.help import *
 from .commands.find_book import *
 from .commands.get_book import *
+from .commands.find_author import *
+from .commands.find_genre import *
