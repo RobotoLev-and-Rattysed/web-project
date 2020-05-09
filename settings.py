ccb110276753c8bc70ab55214b1492099a6a00c0
Programmer <robotolev@gmail.com>
2020-05-09 04:02:54 +0300

    Убрал из settings.py все ключи

--- settings.py
+++ settings.py
@@ -1,16 +1,19 @@
+# ВНИМАНИЕ!
+# Все ключи, находящиеся в публичном доступе, деактивированы.
+# Доступ по ним к чему-либо невозможен.
+
 # Настройки Flask
-csrf_key = 'b0ecS4g2Kh0Wt4C2dhYLt7L38Z8EKl348yP2eDt05kzNP40PRHf86Q7Z36WClN5X6R6siJ960aoHL8qf'
+csrf_key = ''
 
 # Настройки API ВКонтакте
-vk_group_key =\
-    '5996fb84f53d0f5a8c26ec95211f8828b4e8643f60f2793cbf2cc92a6fb94dfe203befd16ff799ec455e9'
-vk_confirmation_key = '6e666245'
-vk_secret_key = 'Vt248qN29CeIsIs4'
+vk_group_key = ''
+vk_confirmation_key = ''
+vk_secret_key = ''
 
 # Настройки API Дискорда
-discord_key = 'NzA4NDM1NTk0NzI4MTc3Nzgz.XrXUog.5FDeTEt2KYsx17y8q4Sn4T1GCLQ'
+discord_key = ''
 
 # Настройки БД на Heroku
 # database_url - только для локального тестирования (осторожно: может устареть)
 # Осторожно: ведет на боевую базу!
-database_url = 'postgres://bttafdnnzcyhyt:ce6d598c047117c17f86bcc4117213d3d8be321a1cc281290c323bf969f3dde1@ec2-54-228-251-117.eu-west-1.compute.amazonaws.com:5432/dcsrq1ikbg262m'
+database_url = ''
